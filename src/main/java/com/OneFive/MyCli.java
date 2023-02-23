ed7e89a976defa6d456e79377fcfc9029f3b73ce
Wu <2321141691@qq.com>
2023-02-23 22:52:34 +0800

    no message

---
+++ src/main/java/com/OneFive/MyCli.java
@@ -0,0 +1,52 @@
+package com.OneFive;
+
+import org.apache.commons.cli.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class MyCli {
+    private final Options options;
+    private final CommandLineParser parser;
+    private CommandLine line;
+    private final Map<Option, Consumer<Object[]>> functionMap;
+
+    private MyCli() {
+        options = new Options();
+        parser = new DefaultParser();
+        functionMap = new HashMap<>();
+        line = null;
+    }
+
+    private static final MyCli instance = new MyCli();
+
+    public static MyCli getInstance() {
+        return instance;
+    }
+
+    public void addOption(Option option, Consumer<Object[]> callback) {
+        options.addOption(option);
+        functionMap.put(option, callback);
+    }
+
+    public void parseAllOptions(String[] args) throws ParseException {
+        if (line == null) {
+            line = getCommandLine(args);
+        }
+        line = parser.parse(options, args);
+        functionMap.forEach((opt, callback) -> {
+            if (line.hasOption(opt.getOpt())) {
+                String[] values = line.getOptionValues(opt);
+                callback.accept(values);
+            }
+        });
+    }
+
+    public CommandLine getCommandLine(String[] args) throws ParseException {
+        if (line != null) {
+            line = parser.parse(options, args);
+        }
+        return line;
+    }
+}
